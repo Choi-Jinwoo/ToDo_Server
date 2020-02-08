85e0145e960bf7cdeee6e078e3e7e833b2ab4684
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-08 16:27:14 +0900

    [INIT] init ts version

--- src/api/v1/token/index.js
+++
@@ -1,8 +0,0 @@
-import * as express from 'express'
-import * as tokenCtrl from './token.ctrl';
-const token = express.Router();
-
-// 토큰 검증 API
-token.get('/', tokenCtrl.validateToken);
-
-export default token;
