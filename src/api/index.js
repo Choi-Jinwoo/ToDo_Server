55efbab2c170e6c163eb8125cc308f747eac58aa
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-08 16:25:28 +0900

    [INIT] todo ts version

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import * as express from 'express'
-const api = express.Router();
-
-import v1 from './v1'
-
-api.use('/v1', v1);
-
-export default api;
--- src/api/v1/user/index.js
+++
@@ -1,9 +0,0 @@
-import * as express from 'express'
-import * as userCtrl from './user.ctrl';
-import authMiddleware from '../../../middleware/auth';
-const user = express.Router();
-
-// 본인 정보 확인 API
-user.get('/', authMiddleware, userCtrl.getUserByToken);
-
-export default user;
