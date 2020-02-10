ee1cb338a6e15e8d2c1ca1063942ed2ddaeab8d1
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-10 14:54:02 +0900

    [ADD] list - delete

--- src/routes/api/v1/list/index.ts
+++ src/routes/api/v1/list/index.ts
@@ -6,5 +6,6 @@ const routes = Router();
 
 routes.get('/:idx', authMiddleware, listCtrl.getListByMenu);
 routes.post('/', authMiddleware, listCtrl.createList);
+routes.delete('/:idx', authMiddleware, listCtrl.deleteList);
 
 export default routes;
